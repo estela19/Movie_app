27060fe9ba9ddc0ebf04aeb3835338610476f795
estela19 <e9857256@naver.com>
2020-07-12 17:54:40 +0900

    Feat : Add redirect home

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 
-function Detail(props){
-    console.log(props);
-    return (
-        <span>hello</span>
-    );
+class Detail extends React.Component{
+    componentDidMount(){
+        const {location, history} = this.props;
+        if(location.state === undefined){
+            history.push('/');
+        }
+    }
+
+    render() {
+        return <span>hi</span>
+    }
 }
+
 
 export default Detail;
